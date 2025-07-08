656927a848a1d94a19d7a3397193aece5bf3b428
Joanne Lin <y3933y3933@gmail.com>
2025-07-08 23:11:58 +0800

    fix: add reverse question description

--- algorithm/002/reverse.go
+++ algorithm/002/reverse.go
@@ -1,5 +1,8 @@
 package reverse
 
+// 請實作一個函式 Reverse(arr []rune)，**就地（in-place）**反轉一個字元陣列 arr。
+// 你必須直接修改輸入陣列的內容（不能建立新的陣列），並且保證空間複雜度為 O(1)。
+
 func Reverse(arr []rune) {
 	left := 0
 	right := len(arr) - 1
